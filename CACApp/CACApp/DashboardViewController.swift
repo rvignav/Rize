2a8c7488c465ef8ab13c70cf18e668b15a9e6a82
ayaanzhaque <ayaanzhaque@gmail.com>
2020-06-22 09:52:38 -0700

    began onboarding

--- CACApp/CACApp/DashboardViewController.swift
+++ CACApp/CACApp/DashboardViewController.swift
@@ -18,7 +18,18 @@ class DashboardViewController: UIViewController {
         self.host.layer.cornerRadius = 25
         self.schedule.layer.cornerRadius = 25
 
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if Core.shared.isNewUser() {
+            // show onboarding
+            
+            
+            
+        }
     }
     
     @IBAction func schedule(_ sender: Any) {
@@ -27,14 +38,24 @@ class DashboardViewController: UIViewController {
     @IBAction func new(_ sender: Any) {
         
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+}
+
+class Core {
+    
+    static let shared = Core()
+    
+    func isNewUser() -> Bool {
+        
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+        
     }
-    */
-
+    
+    func setIsNotNewUser(){
+        
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+        
+    }
+    
 }
---
+++ CACApp/CACApp/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  CACApp
+//
+//  Created by Ayaan Haque on 6/22/20.
+//  Copyright © 2020 Ayaan Haque. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
