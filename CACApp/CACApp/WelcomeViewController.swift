c52368dd09e545a581238e5002f48b1cdfecbe48
rvignav <rvignav@gmail.com>
2020-06-22 15:37:59 -0700

    Fix taglines

--- CACApp/CACApp/WelcomeViewController.swift
+++ CACApp/CACApp/WelcomeViewController.swift
@@ -28,13 +28,13 @@ class WelcomeViewController: UIViewController {
         scrollView.frame = holderView.bounds
         holderView.addSubview(scrollView)
         
-        let titles  = ["Welcome", "Location", "All Set"]
+        let titles  = ["Welcome to Rize.", "Help doctors help you.", "Remember the key points from every appointment."]
         for x in 0..<3{
             let pageView = UIView(frame: CGRect(x: CGFloat(x) * holderView.frame.size.width , y: 0, width: holderView.frame.size.width, height: holderView.frame.size.height))
             
             scrollView.addSubview(pageView)
             
-            let label = UILabel(frame: CGRect(x: 10, y: 10, width: pageView.frame.size.width-20, height: 120))
+            let label = UILabel(frame: CGRect(x: 10, y: 10, width: pageView.frame.size.width-20, height: 400))
             let imageView = UIImageView(frame: CGRect(x: 10, y: 10+120+10, width: pageView.frame.size.width-20, height: pageView.frame.size.height - 60 - 130 - 15))
             let button = UIButton(frame: CGRect(x: 10, y: pageView.frame.size.height-60, width: pageView.frame.size.width-20, height: 50))
 
