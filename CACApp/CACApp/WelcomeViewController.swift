bf0ac7ef2dd9a508008d96c854001b65dd15f34a
ayaanzhaque <ayaanzhaque@gmail.com>
2020-06-22 17:48:19 -0700

    added page control

--- CACApp/CACApp/WelcomeViewController.swift
+++ CACApp/CACApp/WelcomeViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class WelcomeViewController: UIViewController {
     
     @IBOutlet var holderView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
     let scrollView = UIScrollView()
 
     override func viewDidLoad() {
@@ -29,17 +30,21 @@ class WelcomeViewController: UIViewController {
         holderView.addSubview(scrollView)
         
         let titles  = ["Welcome to Rize.", "Help doctors help you.", "Remember the key points from every appointment."]
+        
+        pageControl.numberOfPages = 3
+        
+        
         for x in 0..<3{
             let pageView = UIView(frame: CGRect(x: CGFloat(x) * holderView.frame.size.width , y: 0, width: holderView.frame.size.width, height: holderView.frame.size.height))
-            
+                        
             scrollView.addSubview(pageView)
             
             let label = UILabel(frame: CGRect(x: 10, y: 10, width: pageView.frame.size.width-20, height: 400))
             let imageView = UIImageView(frame: CGRect(x: 10, y: 10+120+10, width: pageView.frame.size.width-20, height: pageView.frame.size.height - 60 - 130 - 15))
-            let button = UIButton(frame: CGRect(x: 10, y: pageView.frame.size.height-60, width: pageView.frame.size.width-20, height: 50))
+            let button = UIButton(frame: CGRect(x: 10, y: pageView.frame.size.height-150, width: pageView.frame.size.width-20, height: 50))
 
             label.textAlignment = .center
-            label.font = UIFont(name: "Helvetica-Bold", size: 32)
+            label.font = UIFont(name: "Helvetica-Bold", size: 18)
             pageView.addSubview(label)
             label.text = titles[x]
             
@@ -74,6 +79,12 @@ class WelcomeViewController: UIViewController {
         }
         
         scrollView.setContentOffset(CGPoint(x: holderView.frame.size.width * CGFloat(button.tag), y: 0), animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView){
+        let page = scrollView.contentOffset.x / scrollView.frame.size.width
+        
+        pageControl.currentPage = Int(page)
     }
     
     
---
+++ CACApp/Pods/Target Support Files/Pods-CACApp/Pods-CACApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CACApp : NSObject
+@end
+@implementation PodsDummy_Pods_CACApp
+@end
