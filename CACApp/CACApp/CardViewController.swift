2f7188e5c6c4db908f6ad6dc946a3980f7d94b33
ayaanzhaque <ayaanzhaque@gmail.com>
2020-06-25 18:04:53 -0700

    began drag up menu

---
+++ CACApp/CACApp/CardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewController.swift
+//  CACApp
+//
+//  Created by Ayaan Haque on 6/25/20.
+//  Copyright © 2020 Ayaan Haque. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var handleView: UIView!
+    
+    
+
+}
--- CACApp/CACApp/DashboardViewController.swift
+++ CACApp/CACApp/DashboardViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class DashboardViewController: UIViewController {
     
+    enum CardState {
+        case expanded
+        case collapsed
+    }
+    
     @IBOutlet weak var host: UIButton!
+    
+    var cardViewController:CardViewController!
+    var visualEffectView:UIVisualEffectView!
+    
+    let cardHeight:CGFloat = 600
+    let cardHandleAreaHeight:CGFloat = 65
+    
+    var cardVisible = false
+    var nextState:CardState {
+        return cardVisible ? .collapsed: .expanded
+    }
+    
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted:CGFloat = 0
     
     @IBOutlet weak var schedule: UIButton!
     override func viewDidLoad() {
@@ -18,9 +37,26 @@ class DashboardViewController: UIViewController {
         title = "Home"
         self.host.layer.cornerRadius = 25
         self.schedule.layer.cornerRadius = 25
-        
+        setupCard()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setupCard(){
+        visualEffectView = UIVisualEffectView()
+        visualEffectView.frame = self.view.frame
+        self.view.addSubview(visualEffectView)
+        
+        cardViewController = CardViewController(nibName: "CardViewController", bundle: nil)
+        self.addChild(cardViewController)
+        self.view.addSubview(cardViewController.view)
+        
+        cardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width, height: cardHeight)
+        
+        cardViewController.view.clipsToBounds = true
+        
+        
+        
     }
     
     @IBAction func schedule(_ sender: Any) {
@@ -29,14 +65,5 @@ class DashboardViewController: UIViewController {
     @IBAction func new(_ sender: Any) {
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
