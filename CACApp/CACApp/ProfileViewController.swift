eeed3e21353a809ff9cc6615febbb8443e24e7da
ayaanzhaque <ayaanzhaque@gmail.com>
2020-07-06 22:32:57 -0700

    finished nav bar, formatted UI

--- CACApp/CACApp/ProfileViewController.swift
+++ CACApp/CACApp/ProfileViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ProfileViewController: UIViewController {
     @IBOutlet weak var profile: UIImageView!
     
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var transcriptView: UIView!
+    
     @IBAction func segmentedControl(_ sender: UISegmentedControl) {
-//        switch sender.selectedSegmentIndex {
-//        case 0:
-//            <#code#>
-//        default:
-//            <#code#>
-//        }
+        switch sender.selectedSegmentIndex {
+        case 0:
+            profileView.isHidden = false
+            transcriptView.isHidden = true
+        default:
+            profileView.isHidden = true
+            transcriptView.isHidden = false
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +30,8 @@ class ProfileViewController: UIViewController {
         
         self.profile.layer.cornerRadius = 25
         
+        profileView.isHidden = false
+        transcriptView.isHidden = true
 
     }
 
