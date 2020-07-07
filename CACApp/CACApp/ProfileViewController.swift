e77b39733bd9355a64c9ced07f8b983df47ab5c0
ayaanzhaque <ayaanzhaque@gmail.com>
2020-07-07 11:16:23 -0700

    finished profile navbar

--- CACApp/CACApp/ProfileViewController.swift
+++ CACApp/CACApp/ProfileViewController.swift
@@ -12,11 +12,27 @@ import FirebaseAuth
 class ProfileViewController: UIViewController {
     @IBOutlet weak var profile: UIImageView!
     
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var transcriptView: UIView!
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            profileView.isHidden = false
+            transcriptView.isHidden = true
+        default:
+            profileView.isHidden = true
+            transcriptView.isHidden = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
         
-        self.profile.layer.cornerRadius = 25
+//        self.profile.layer.cornerRadius = 25
+        
+        profileView.isHidden = false
+        transcriptView.isHidden = true
+        
 
     }
     
