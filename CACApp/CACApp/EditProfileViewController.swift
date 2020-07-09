dea93110f5b59945e914b4c935adcaa4204f674a
ayaanzhaque <ayaanzhaque@gmail.com>
2020-07-09 10:38:24 -0700

    added UI elements

---
+++ CACApp/CACApp/EditProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EditProfileViewController.swift
+//  CACApp
+//
+//  Created by Ayaan Haque on 7/9/20.
+//  Copyright © 2020 Ayaan Haque. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var doctorField: UITextField!
+    @IBOutlet weak var symptomsField: UITextField!
+    @IBOutlet weak var conditionsField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    
+    @IBAction func uploadPicture(_ sender: Any) {
+        
+        
+        
+    }
+    
+}
